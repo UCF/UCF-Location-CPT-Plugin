880044af7ef3fad733c6fdb1e470d0666fcbafc5
Jim Barnes <jimbarnesdeveloper@gmail.com>
2019-08-28 11:07:17 -0400

    Updated readmes

---
+++ ucf-location-cpt.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Location Custom Post Type
+Description: Provides a custom post type and custom fields for describing locations.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/UCF-Location-CPT-Plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
