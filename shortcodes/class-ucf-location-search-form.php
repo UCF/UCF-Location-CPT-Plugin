cdffb2db363908697acd48f09bb96f9fc87db78b
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-01-04 16:19:43 -0500

    Added a search form shortcode

---
+++ shortcodes/class-ucf-location-search-form.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Provides for a traditional form for searching
+ * for locations.
+ */
+if ( ! class_exists( 'UCF_Location_Search_Shortcode' ) ) :
+
+class UCF_Location_Search_Shortcode {
+	/**
+	 * Registers the `location-search` shortcode
+	 * @author Jim Barnes
+	 * @since 0.3.0
+	 */
+	public static function register_shortcode() {
+		add_shortcode( 'location-search', array( 'UCF_Location_Search_Shortcode', 'callback' ) );
+	}
+
+	/**
+	 * The callback function when the `location-search`
+	 * shortcode is used.
+	 * @author Jim Barnes
+	 * @since 0.3.0
+	 * @param array $atts The array of attributes passed into the shortcode
+	 * @param string $content The content contained within the shortcode
+	 * @return string The HTML output as a string
+	 */
+	public static function callback( $atts, $content = '' ) {
+		$q = isset( $_GET['q'] ) ? sanitize_text_field( $_GET['q'] ) : null;
+
+		$atts = shortcode_atts(
+			array(
+				'layout'         => 'location',
+				'post_type'      => 'location',
+				'order'          => 'ASC',
+				'order_by'       => 'post_title',
+				'posts_per_page' => -1,
+				'posts_per_row'  => 2
+			),
+			$atts
+		);
+
+		// Add the query
+		if ( ! empty( $q ) ) $atts['s'] = $q;
+
+		$parts = array();
+
+		foreach ( $atts as $key => $val  ) {
+			$parts[] = $key . "=\"" . $val . "\"";
+		}
+
+		$att_str = implode( ' ', $parts );
+
+		ob_start();
+	?>
+		<form action="." type="get">
+			<input type="text" class="form-control form-control-lg form-control-search" id="location_search_q" name="q" placeholder="Library" aria-label="Search UCF Locations"<?php echo ( ! empty( $q ) ) ? ' value="' . $q . '"' : '';?>>
+			<input type="submit" class="btn btn-primary" value="Submit">
+		</form>
+		<?php echo do_shortcode('[ucf-post-list ' . $att_str . ']'); ?>
+	<?php
+		return ob_get_clean();
+	}
+}
+
+endif;
